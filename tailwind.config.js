82f7cb39031f63470500762e2d5057e7e39f0026
doanduyvan <duyvanlee2001@gmail.com>
2024-11-06 07:58:03 +0700

    update

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./public/**/*.{html,js}", "./src/mvc/Views/Users/login/**/*.{js,php}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
