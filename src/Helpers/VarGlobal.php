98de09ff896b01839a9498c1710ce8bf52ac00d9
doanduyvan <duyvanlee2001@gmail.com>
2024-12-06 19:57:22 +0700

    change web_root

--- src/Helpers/VarGlobal.php
+++ src/Helpers/VarGlobal.php
@@ -17,7 +17,9 @@ $web_root .= '/' . implode("/",$arrPath) . "" ;
 // variable global
 
 // $web_root = 'http://localhost/duan1_3/';
-$web_root = 'http://localhost:8085/';
+// $web_root = 'http://localhost:8085/';
+$web_root = 'http://vps.fptduyvan.id.vn:8085/';
+
 
 
 define("WEB_ROOT", $web_root);
